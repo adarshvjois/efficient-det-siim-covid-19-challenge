8c61567167d9c43cd7089a4b295864797fa3e2ce
adarshvjois <adarshmcool@gmail.com>
2021-09-27 12:15:00 +0530

    Fixed Eval code and added optimizations to csv creation

--- Extract_Images_NFL.py
+++ Extract_Images_NFL.py
@@ -4,18 +4,23 @@
 # In[1]:
 
 
+from pandarallel.pandarallel import PREFIX
 import pandas as pd
 import glob
 import numpy
 import multiprocessing
 import os
 import cv2
-NFL_ROOT_DIR = "C:/Users/Kashyap/bkp/source/repos/efficient-det-siim-covid-19-challenge/kaggle-nfl/nfl-health-and-safety-helmet-assignment/"
-TRAINING_DATA = os.path.join(NFL_ROOT_DIR,"train_labels.csv")
-VIDEO_DIR = os.path.join(NFL_ROOT_DIR,"train")
-NUMBER_OF_POOLS = 4
-SIZE = (512, 512)
 
+NFL_ROOT_DIR = "/home/puttuboo/Datasets/nfl-health-and-safety-helmet-assignment"
+PREFIX = "train"
+
+TRAINING_DATA = os.path.join(NFL_ROOT_DIR, f"{PREFIX}_labels.csv")
+
+VIDEO_DIR = os.path.join(NFL_ROOT_DIR, PREFIX)
+NUMBER_OF_POOLS = 4
+SIZE = (1024, 1024)
+OUTPUT_DIR = os.path.join(NFL_ROOT_DIR, PREFIX + "_frames")
 
 # In[10]:
 
@@ -28,38 +33,30 @@ label_per_image = train_df.groupby("video_frame")
 # In[8]:
 
 
-def process(file_path,prefix="train"):
+def process(file_path, prefix="train"):
     basename = os.path.basename(file_path).split(".")[0]
-    print("Processing file .." +basename)
-    cam = cv2.VideoCapture(file_path)  
-    output_dir = os.path.join(NFL_ROOT_DIR,prefix+'_frames')
-    try:  
-        # creating a folder named data
-        if not os.path.exists(output_dir):
-            os.makedirs(os.path.join(output_dir))
-        # if not created then raise error
-    except OSError:
-        print ('Error: Creating directory of data')
-  
+    print("Processing file .." + basename)
+    cam = cv2.VideoCapture(file_path)
 
     currentframe = 0
-  
-    while(True):
-      
+
+    while True:
+
         # reading from frame
-        ret,frame = cam.read()
+        ret, frame = cam.read()
 
         if ret:
-            currentframe += 1
+
             # if video is still left continue creating images
-            name = os.path.join(output_dir,basename+"_"+str(currentframe)+'.jpg')  
+            name = os.path.join(OUTPUT_DIR, basename + "_" + str(currentframe) + ".jpg")
+            currentframe += 1
             # writing the extracted images
-            frame = cv2.resize(frame,SIZE)
+            frame = cv2.resize(frame, SIZE)
             cv2.imwrite(name, frame)
 
             # increasing counter so that it will
             # show how many frames are created
-            
+
         else:
             break
 
@@ -67,25 +64,20 @@ def process(file_path,prefix="train"):
     cam.release()
     cv2.destroyAllWindows()
 
+
 # In[9]:
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     p = multiprocessing.Pool(NUMBER_OF_POOLS)
-    files = [f for f in list(glob.glob(VIDEO_DIR+os.sep+"*mp4"))]
-    p.map(process,files)
+    
+    try:
+        # creating a folder named data
+        if not os.path.exists(OUTPUT_DIR):
+            os.makedirs(os.path.join(OUTPUT_DIR))
+        # if not created then raise error
+    except OSError:
+        print("Error: Creating directory of data")
+    files = [f for f in list(glob.glob(VIDEO_DIR + os.sep + "*mp4"))]
+    p.map(process, files)
     p.close()
     p.join()
-    
-
-
-# In[15]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
